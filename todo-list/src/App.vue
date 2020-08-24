c1ea48299a9f94263985a444fa1a70d22d7fb34f
Marcelo Mendes <ti.marcelomendes@gmail.com>
2020-08-23 22:22:45 -0300

    Alter App Vue

--- todo-list/src/App.vue
+++ todo-list/src/App.vue
@@ -6,7 +6,7 @@
         <h2 class="ui inverted header">Marcelo Mendes
           <div class="sub header">Negócios em TI @ Memora</div>
         </h2>
-        <h3 class="ui inverted header">Rio de Janeiro, Brasil - 2020</h3>
+        <h3 class="ui inverted header">Goiânia, Brasil - 2020</h3>
         <a class="tiny ui youtube button" type="button" href="https://www.youtube.com/user/l32759">
           <i class="youtube icon"></i>
           YouTube
